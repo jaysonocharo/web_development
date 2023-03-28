0bdcc5c77c85a22675fbd358c111892ccdedf67b
jaysonocharo <mogakajayson@gmail.com>
2023-03-28 10:57:55 +0300

    iportfolio

---
+++ javascript/index.js
@@ -0,0 +1,20 @@
+let num = 20
+if(num<10){
+    //code if condition is true
+    console.log("number is less than 10")
+}
+else{
+    console.log("number is greater than 10")
+}
+//rest of the code.
+num_1=15
+if (num_1>10){
+    console.log("greater than 20")
+}
+else if(num_1==10){
+    console.log("less than 10")
+}
+country="Chad"
+town="Abuja"
+if(country=="Chad" && town=="Abuja")
+console.log("nah fahm")
